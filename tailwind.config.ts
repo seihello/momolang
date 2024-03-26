44e321c70c79e99ef07b957887b9d3515ddb1e1c
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-03-25 20:22:52 -0700

    chore: set up the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
@@ -7,14 +8,46 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["var(--font-sans)", ...fontFamily.sans],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          100: "#F1FBFB",
+          300: "#CEF1F1",
+          500: "#86CED8",
+          700: "#35AEBE",
+          900: "#098292",
+        },
+        warning: {
+          100: "#FFF4E4",
+          500: "#D9CA7B",
+          900: "#9E8E00",
+        },
+        error: {
+          100: "#FFEEEF",
+          500: "#F55C67",
+          900: "#DD224F",
+        },
+        gray: {
+          100: "#F3F5F7",
+          200: "#E2E5E7",
+          300: "#D4D7DD",
+          400: "#CBCBCF",
+          500: "#ABAFB5",
+          600: "#989DA5",
+          700: "#747A81",
+          800: "#5A5F62",
+          900: "#1F2324",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // require("tailwindcss-animate"),
+    require("autoprefixer"),
+    require("tailwindcss-text-fill"),
+  ],
 };
 export default config;
