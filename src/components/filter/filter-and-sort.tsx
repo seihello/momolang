4eff6fe0882569fc4b88f840d67e7d033f4822a9
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-03-28 00:41:32 -0700

    style: remove space from the filter box

--- src/components/filter/filter-and-sort.tsx
+++ src/components/filter/filter-and-sort.tsx
@@ -64,7 +64,7 @@ export default function FilterAndSort({
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-auto overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+        <Listbox.Options className="absolute z-10 mt-1 max-h-64 w-auto overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
           {compare && setItems && (
             <div className="flex items-stretch border-b">
               <button
